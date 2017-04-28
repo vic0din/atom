a0a26457f2a76bbdf7806ae9a8a4caaa48aa701c
Egor <rentgen.94@mail.ru>
2017-04-29 00:12:28 +0400

    try to understand our task and program logic

--- homeworks/HW3/src/main/java/ru.atom/websocket/server/EventServer.java
+++ homeworks/HW3/src/main/java/ru.atom/websocket/server/EventServer.java
@@ -6,28 +6,42 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class EventServer {
+    private static Server eventServer;
+
     public static void main(String[] args) {
-        Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
+        startEventServer();
+    }
+
+    public static void startEventServer() {
+        eventServer = new Server();
+        ServerConnector connector = new ServerConnector(eventServer);
         connector.setPort(8090);
-        server.addConnector(connector);
+        eventServer.addConnector(connector);
 
         // Setup the basic application "context" for this application at "/"
         // This is also known as the handler tree (in jetty speak)
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        server.setHandler(context);
+        eventServer.setHandler(context);
 
         // Add a websocket to a specific path spec
         ServletHolder holderEvents = new ServletHolder("ws-events", EventServlet.class);
         context.addServlet(holderEvents, "/events/*");
 
         try {
-            server.start();
-            server.dump(System.err);
-            server.join();
+            eventServer.start();
+            eventServer.dump(System.err);
+            eventServer.join();
         } catch (Throwable t) {
             t.printStackTrace(System.err);
+        }
+    }
+
+    public static void stopEventServer() {
+        try {
+            eventServer.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ homeworks/HW3/src/test/java/ru/atom/ConnectionPoolTest.java
@@ -0,0 +1,78 @@
+package ru.atom;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.junit.Test;
+import ru.atom.model.GameSession;
+import ru.atom.model.Movable;
+import ru.atom.websocket.message.Message;
+import ru.atom.websocket.message.Topic;
+import ru.atom.websocket.network.Broker;
+import ru.atom.websocket.network.ConnectionPool;
+import ru.atom.websocket.server.EventHandler;
+import ru.atom.websocket.util.JsonHelper;
+
+import java.net.URI;
+import java.util.concurrent.Future;
+
+/**
+ * Тест для понимания логики программы
+ * Created by Egor Shchurbin on 28.04.2017.
+ */
+public class ConnectionPoolTest {
+//    @Before
+//    public void eventServerInitializer() {
+//        EventServer.startEventServer();
+//    }
+
+    @Test
+    public void SendingMessageForServer() {
+        URI uri = URI.create("ws://localhost:8090/events/");
+
+        WebSocketClient client = new WebSocketClient();
+        GameSession gameSession = new GameSession();
+        try {
+            try {
+                client.start();
+                // The socket that receives events
+                EventHandler socket = new EventHandler();
+                // Attempt Connect
+                Future<Session> fut = client.connect(socket, uri);
+                // Wait for Connect
+                Session session = fut.get();
+
+                //TODO Разделить логику клиента и сервера
+                // Клиентская часть
+                // В игру заходит нужное количество человек
+                ConnectionPool.getInstance().add(session, "FirstPlayer");
+                ConnectionPool.getInstance().add(session, "SecondPlayer");
+                ConnectionPool.getInstance().add(session, "ThirdPlayer");
+                ConnectionPool.getInstance().add(session, "FourthPlayer");
+                // В идеале, на этом этапе брокер получает команды от пользователей из фронтенда
+                Message msg = new Message(Topic.MOVE, JsonHelper.toJson(Movable.Direction.UP));
+                Broker.getInstance().send("FirstPlayer", msg.getTopic(), msg.getData());
+
+                //TODO Разделить логику клиента и сервера
+                // Серверная часть
+                // Получаем данные
+                //TODO Понять как получить данные от фронта
+                Broker.getInstance().receive(session, JsonHelper.toJson(msg));
+                // В этом месте происходят всякие серверные штучки
+                // Просиходит тик в модели
+                gameSession.tick(1000L);
+                // Отправка изменений клиентам
+                Broker.getInstance().broadcast(Topic.MOVE, gameSession.getGameObjects());
+
+            } finally {
+                client.stop();
+            }
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+//    @After
+//    public void eventServerDisabler() {
+//        EventServer.startEventServer();
+//    }
+}
